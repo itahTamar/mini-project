cd83737edc593ec0a4ffb7c9fa364d837a1ed2a9
itahTamar <125893355+itahTamar@users.noreply.github.com>
2024-01-15 10:16:33 +0200

    update filders name

--- books/claient/src/router/router.tsx
+++ books/claient/src/router/router.tsx
@@ -1,6 +1,6 @@
 import SpecificBookPage from "@/components/books/SpecificBookPage";
 import { createBrowserRouter } from "react-router-dom";
-import BooksPage from './../components/books/BooksPage';
+import BooksPage from '../components/books/BooksPage';
 
 export const router = createBrowserRouter([
     {path: "", element: <BooksPage/> },
