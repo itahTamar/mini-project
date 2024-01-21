de2b334f1a8ffd39c5a3a83866c68d192347f316
itahTamar <tamar.verber@gmail.com>
2024-01-21 17:32:40 +0200

    update

--- books/claient/src/component/books/AddBook.tsx
+++ books/claient/src/component/books/AddBook.tsx
@@ -30,13 +30,14 @@ const handleSubmit = async (ev: React.FormEvent<HTMLFormElement>) => {
 
   return (
     <form className="addBookForm" onSubmit={handleSubmit}>
-        <input type='text'  placeholder='title' value={title} onInput={(ev) => setTitle((ev.target as HTMLInputElement).value)}></input>
-        <input type='text'  placeholder='author' value={author} onInput={(ev) => setAuthor((ev.target as HTMLInputElement).value)}></input>
+        <input type='text'  placeholder='*title' value={title} onInput={(ev) => setTitle((ev.target as HTMLInputElement).value)}></input>
+        <input type='text'  placeholder='*author' value={author} onInput={(ev) => setAuthor((ev.target as HTMLInputElement).value)}></input>
         <input type='number'  placeholder='page_num' value={page_num} onInput={(ev) => setPage_num(Number((ev.target as HTMLInputElement).value))}></input>
         <input type='text'  placeholder='publisher' value={publisher} onInput={(ev) => setPublisher((ev.target as HTMLInputElement).value)}></input>
-        <input type='text'  placeholder='description' value={description} onInput={(ev) => setDescription((ev.target as HTMLInputElement).value)}></input>
-        <input type='text'  placeholder='image URL' value={image} onInput={(ev) => setImage((ev.target as HTMLInputElement).value)}></input>
+        <input type='text'  placeholder='*description' value={description} onInput={(ev) => setDescription((ev.target as HTMLInputElement).value)}></input>
+        <input type='text'  placeholder='*image URL' value={image} onInput={(ev) => setImage((ev.target as HTMLInputElement).value)}></input>
         <input type='text'  placeholder='genre' value={genre} onInput={(ev) => setGenre((ev.target as HTMLInputElement).value)}></input>
+        <p>The filed with * are mandatory</p>
         <button type='submit'>Add your book</button>
         <button onClick={() => navigate("/booksPage")}>Back</button>
     </form>
