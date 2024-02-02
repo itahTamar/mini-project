242cbcd94c71e4d4ac706b1746973a2e186c164c
itahTamar <tamar.verber@gmail.com>
2024-02-02 10:27:36 +0200

    update

--- books/claient/src/App.tsx
+++ books/claient/src/App.tsx
@@ -1,6 +1,12 @@
 import {RouterProvider} from 'react-router-dom'
 import {router} from './router/router'
 import './App.css'
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
+
+let environment = "PROD"
+
+environment === "DEV" ? process.env.SERVER_URL_DEV : process.env.SERVER_URL_PROD
+disableReactDevTools()
 
 function App() {
 
